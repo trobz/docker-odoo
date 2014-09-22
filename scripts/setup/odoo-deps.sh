fcb202a452df6b95bbc3c6cda2f1eb59a385582e
Michel Meyer <michel@zazabe.fr>
2014-09-22 16:29:58 +0700

    remove most of apt-get python dependencies and trust odoo setup.py file

--- scripts/setup/odoo-deps.sh
+++ scripts/setup/odoo-deps.sh
@@ -5,11 +5,6 @@ echo "APT: install Odoo default libs"
 echo ttf-mscorefonts-installer	msttcorefonts/accepted-mscorefonts-eula	boolean	true | /usr/bin/debconf-set-selections
 
 apt-get install -y \
-    python-dateutil python-feedparser python-gdata python-ldap \
-    python-libxslt1 python-lxml python-mako python-openid python-psycopg2 \
-    python-pybabel python-pychart python-pydot python-pyparsing python-reportlab \
-    python-simplejson python-tz python-vatnumber python-vobject python-webdav \
-    python-werkzeug python-xlwt python-yaml python-zsi \
     python-pip python-dev \
     fontconfig fontconfig-config graphviz ghostscript gsfonts ttf-mscorefonts-installer
 
