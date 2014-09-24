0fe5bc88f64de91f4b85b27e10ed77522fefd465
Michel Meyer <mmeyer@trobz.com>
2014-09-24 13:44:18 +0200

    use odoo github source instead of the nightly build

--- scripts/start/init/40_demo_odoo.sh
+++ scripts/start/init/40_demo_odoo.sh
@@ -3,14 +3,16 @@ if [[ $ODOO_DEMO -eq 1 ]]; then
 
     info 'Setup a Odoo 7.0 demo instance'
     
-    debug 'temporary start postgres service'
-
     DEMODB="demo_odoo70"
-    PGPATH="/etc/postgresql/docker/" 
+    PGPATH="/etc/postgresql/docker" 
+    OOPATH="/usr/local/lib/odoo"
     PGEXEC="/usr/lib/postgresql/9.1/bin"
-    OOEXEC="/usr/local/lib/odoo/openerp-7.0/openerp-server"
-    PGPIDFILE="/var/run/postgresql/9.1-main.pid"
+    OOEXEC="$OOPATH/openerp-server"
 
+    info 'Update Odoo 7.0 source code'
+    cd $OOPATH && git pull origin 7.0
+
+    debug 'temporary start postgres service'
     sudo -u postgres -H $PGEXEC/pg_ctl start -w -D $PGPATH/data/db &>/dev/null
         
     set +e ; sudo -u $USERNAME -H psql -l | grep $DEMODB &>/dev/null ; DB_EXISTS=$? ; set -e
