95f2eced4c0c2138cc431d1ac66b29cb40dd8854
Michel Meyer <mmeyer@trobz.com>
2014-10-14 14:03:13 +0200

    remove odoo sources from pip packages added to sys.path to avoid conflict with other possible odoo source in dev mode

--- scripts/setup/odoo.sh
+++ scripts/setup/odoo.sh
@@ -5,4 +5,11 @@ echo "Clone Odoo 7.0 from github and setup it with pip"
 git clone -b 7.0 https://github.com/odoo/odoo.git /usr/local/lib/odoo
 cd /usr/local/lib/odoo && pip install -ve .
 
+# remove /usr/local/lib/odoo pip reference to avoid conflicts with other odoo source
+TMP=$(mktemp)
+TARGET="/usr/local/lib/python2.7/dist-packages/easy-install.pth"
+cat "$TARGET" | grep -v "/usr/local/lib/odoo" > "$TMP"
+mv "$TMP" "$TARGET"
+chmod +r "$TARGET"
+
 echo "Odoo 7.0 setup done !"
