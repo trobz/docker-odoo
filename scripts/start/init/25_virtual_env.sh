ca92269e1681e1f324e82a0a808926bcc75c25bc
Michel Meyer <mmeyer@trobz.com>
2015-06-18 12:03:19 +0200

    fix openoffice uno python package issue inside odoo-7.0 virtual env

--- scripts/start/init/25_virtual_env.sh
+++ scripts/start/init/25_virtual_env.sh
@@ -5,7 +5,9 @@ info "Create default virtual env for user ${USERNAME}"
 export VENV="odoo-7.0"
 export VENV_PATH="${USER_HOME}/.local/share/virtualenvs/${VENV}"
 
-sudo -u "$USERNAME" -H pew new odoo-7.0 -d
+sudo -u "$USERNAME" -H pew new "$VENV" -d
 sudo -u "$USERNAME" -H pew in "$VENV" pip install --use-wheel --no-index --find-links=/tmp/setup/odoo/wheel -r /tmp/setup/odoo/requirements.txt
+sudo -u "$USERNAME" -H pew in "$VENV" pew add /usr/share/pyshared
+
 
 success "Virtual env $VENV created for ${USERNAME}"
