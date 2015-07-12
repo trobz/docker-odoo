daa705e268dc0ab4225d4bfcc492e9f4351750df
Michel Meyer <mmeyer@trobz.com>
2015-07-12 23:54:23 +0200

    #2 - fix wkhtmltox source

--- scripts/setup/odoo/deps.sh
+++ scripts/setup/odoo/deps.sh
@@ -5,7 +5,7 @@ echo "Install webkit2pdf command"
 WTH_VERSION="0.12.1"
 UBUNTU_ARCHI=`uname -a | grep 64 &>/dev/null; [ $? -eq 0 ] && echo 'amd64' || echo 'i386'`
 UBUNTU_CODE=`lsb_release -a 2>/dev/null | grep Codename  | sed 's/.*\:\t*//'`
-WTH_BASE_URL="http://sourceforge.net/projects/wkhtmltopdf/files/archive/%s/wkhtmltox-%s_linux-%s-%s.deb/download"
+WTH_BASE_URL="http://download.gna.org/wkhtmltopdf/0.12/%s/wkhtmltox-%s_linux-%s-%s.deb/download"
 WTH_URL=`printf $WTH_BASE_URL $WTH_VERSION $WTH_VERSION $UBUNTU_CODE $UBUNTU_ARCHI`
 
 echo "- download wkhtmltopdf $WTH_VERSION deb package for ubuntu $UBUNTU_CODE..."
