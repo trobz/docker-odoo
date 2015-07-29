2b4ff3ad43d5580bb4a0b9b69853197f82575264
Thuan (Duong Tien) <thuan@trobz.com>
2015-07-29 09:48:43 +0700

    Revert wkhtmltox to 0.12.1

--- scripts/setup/odoo/deps.sh
+++ scripts/setup/odoo/deps.sh
@@ -2,7 +2,7 @@ set -e
 
 echo "Install webkit2pdf command"
 
-WTH_VERSION="0.12.2.1"
+WTH_VERSION="0.12.1"
 UBUNTU_ARCHI=`uname -a | grep 64 &>/dev/null; [ $? -eq 0 ] && echo 'amd64' || echo 'i386'`
 UBUNTU_CODE=`lsb_release -a 2>/dev/null | grep Codename  | sed 's/.*\:\t*//'`
 WTH_BASE_URL="http://download.gna.org/wkhtmltopdf/0.12/%s/wkhtmltox-%s_linux-%s-%s.deb"
